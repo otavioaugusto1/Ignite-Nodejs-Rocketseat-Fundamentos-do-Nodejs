2d98cacb99b6845d767c5e32f1ca88f8befb935d
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-10-18 15:53:56 -0300

    Criando a estrutura do projeto

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+//localhost:3333
+app.get("/", (request, response) =>{
+    return response.json({message: "Hello world, ignite!"})
+})
+
+app.listen(3333) // startar nossa aplicação
